eaef20587eb5c777215347445b07a3fb4caa82b8
Ikenna Richard <oguejioforrichard@gmail.com>
2023-01-14 23:43:27 -1100

    Add color theme feature

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -9,9 +9,9 @@ export default function Greeting({messages}) {
   return (
     <div> 
       <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>
+      {/* <button onClick={() => setGreeting(randomMessage())}>
         New Greeting
-      </button>
+      </button> */}
     </div>
   );   
 }
